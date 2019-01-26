7ece691f4526df78fe91552fbb50a1d13f7e9712
Simon Huang <simon@simonhuang.ca>
2019-01-26 12:26:53 -0500

    Index progress

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
 app.use(express.json());
 
+//For static files
+app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
     res.sendStatus(200);
 });
